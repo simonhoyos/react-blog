acdb4cdc919167f644a95c296a0a41ccb931a3fb
Simon Hoyos <shmesa23@gmail.com>
2018-02-13 16:17:55 -0500

    Create input component

---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid #333;
+  margin-bottom: 10px;
+  height: 50px;
+  font-size: 25px;
+  padding-left: 15px;
+`;
+
+export default Input;
