1b3a376defc6ac811b468ca0e00530a4c1d16519
Simon Hoyos <shmesa23@gmail.com>
2018-02-13 08:15:37 -0500

    Create Post component

---
+++ src/components/Post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 2px solid #333;
+  box-sizing: border-box;
+  flex-grow: 1;
+  margin: 10px;
+  min-width: 280px;
+  padding: 0 20px;
+  width: 30%;
+`;
+
+const Post = ({ title, body }) => {
+  return (
+    <Container>
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </Container>
+  );
+};
+
+export default Post;
