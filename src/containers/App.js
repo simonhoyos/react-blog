9b8e9b38cbca74aee79e78800c79bade9252685e
Simon Hoyos <shmesa23@gmail.com>
2018-02-15 14:56:14 -0500

    New styles and components

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import PostForm from './PostForm';
 import Posts from './Posts';
 import styled from 'styled-components';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Hero from '../components/Hero'
+import NavBar from '../components/NavBar';
 
 const Container = styled.div`
   max-width: 980px;
@@ -11,13 +14,29 @@ const Container = styled.div`
 class App extends Component {
   render() {
     return (
-      <Container>
-        <h1>React blog</h1>
-        <h2>Create post: </h2>
-        <PostForm />
-        <h2>Posts: </h2>
-        <Posts />
-      </Container>
+      <div>
+        <Hero header="React Blog" />
+        <BrowserRouter>
+        <div>
+          <NavBar />
+          <Container>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                component={Posts}
+              />
+              <Route
+                path="/create"
+                exact
+                component={PostForm}
+              />
+              <Redirect to="/" />
+            </Switch>
+          </Container>
+        </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
